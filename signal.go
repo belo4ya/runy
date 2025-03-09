39b5e6670dc2d4b197d596952865d4e8b514778d
belo4ya <41exey.kov41ev@gmail.com>
2025-03-10 01:27:16 +0300

    enhance signal handler
    Signed-off-by: belo4ya <41exey.kov41ev@gmail.com>

--- signal.go
+++ signal.go
@@ -7,19 +7,23 @@ import (
 	"syscall"
 )
 
+var onlyOneSignalHandler = make(chan struct{})
+
 // SetupSignalHandler registers for SIGINT and SIGTERM.
 // A context is returned which is canceled on one of these signals.
 // If a second signal is caught, the program is terminated with exit code 1.
 func SetupSignalHandler() context.Context {
+	close(onlyOneSignalHandler) // panics when called twice
+
 	ctx, cancel := context.WithCancel(context.Background())
 
-	ch := make(chan os.Signal, 2)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		<-ch
+		<-c
 		cancel()
-		<-ch
-		os.Exit(1)
+		<-c
+		os.Exit(1) // second signal, exit directly
 	}()
 
 	return ctx
